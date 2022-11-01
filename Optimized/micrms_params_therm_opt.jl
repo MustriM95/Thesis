0b4512b2e993220e115a13c239ea356e7a1aaa5e
Michael <michael.mustri21@imperial.ac.uk>
2022-11-01 17:34:23 -0600

    Added therm model generator and integrator

--- Optimized/micrms_params_therm_opt.jl
+++ Optimized/micrms_params_therm_opt.jl
@@ -3,10 +3,38 @@ This function creates a random set of parameters for the MiCRM.
 Parameters are constrained by thermal dissipation d.
 
 """
+def_ρ(N, M, kw) = ones(M)
+def_ω(N, M, kw) = ones(M)
 
-function def_l(N, M, kw)
+function def_m(N, M, kw)
+
+    if !haskey(kw, :T)
+        T = fill(1.0, N)
+    else
+        T = kw[:T]
+    end
+
+    m = [T[i]^(2/3) for i in 1:N]
+    return m
+end
+
+
+function def_l(N, M, d, kw)
     L = zeros(N, M)
     l = zeros(N, M, M)
+    effD = Uniform(0.1, 0.85)
+
+    if !haskey(kw, :η)
+        η = rand(effD, N, M)
+    else
+        η = kw[:η]
+    end
+
+    if !haskey(kw, :T)
+        T = fill(1.0, N)
+    else
+        T = kw[:T]
+    end
 
     for i in 1:N
         for α in 1:M
@@ -15,12 +43,55 @@ function def_l(N, M, kw)
     end
 
     ## Generate leakage tensor from dirichlet distribution
+    ϕ = fill(1.0, M)
+    dD = Dirichlet(ϕ[:])
     for i in 1:N
-        dD = Dirichlet(Ω[i]*θ[i,:])
         for α in 1:M
             l[i, α, :] = rand(dD)*L[i, α]
         end
     end
+    return l
+end
+
+function F_u(N, M, kw)
+
+    dU = Uniform(0, 1)
+    u = zeros(N, M)
+
+    if !haskey(kw, :θ)
+        θ = zeros(N, M)
+
+        for i in 1:N
+            θ[i, :] = rand(dU, M)
+        end
+    else
+        θ = kw[:θ]
+    end
+
+
+    if !haskey(kw, :Ω)
+    ## Sample specialisation parameter for each consumer
+        Ω = fill(1.0, N)
+    else
+        Ω = kw[:Ω]
+    end
+
+    ## Sample total uptake capacity per consumer
+    if !haskey(kw, :T)
+        T = fill(1.0, N)
+    else
+        T = kw[:T]
+    end
+
+    ## Generate uptake matrix from a dirichlet distribution
+    for i = 1:N
+        dD = Dirichlet(Ω[i] * θ[i, :])
+        u[i, :] = rand(dD) * T[i]
+    end
+    return u
+
+end
+
 
 
 function MiCRM_par_therm(N, M; f_u=def_u, f_l=def_l, f_ρ=def_ρ, f_ω=def_ω, f_m=def_m, kwargs...)
@@ -29,83 +100,38 @@ function MiCRM_par_therm(N, M; f_u=def_u, f_l=def_l, f_ρ=def_ρ, f_ω=def_ω, f
 
     kw = Dict{Symbol, Any}(kwargs)
 
-    u = zeros(N, M)
-    ρ = zeros(M, 1)
-    ω = zeros(M, 1)
-    m = zeros(N, 1)
-
-    ## Define preliminary variables to build uptake matrix and populate uᵢα
-    Ω = zeros(N, 1)
-    T = zeros(N, 1)
-
-    if d == nothing
-        d = rand(N, M)
+    if !haskey(kw, :d)
+        dissD = Uniform(0.0, 0.05)
+        d = rand(dissD, N, M)
     else
-        dNorm = Normal(d, d*0.1)
-        d = rand(dNorm, N, M)
+        d = kw[:d]
     end
 
-    if η == nothing
-        η = rand(N, M)
-    else
-        ηNorm = Normal(η, η*0.1)
-        η = rand(ηNorm, N, M)
-    end
+    ρ = f_ρ(N, M, kw)
+    ω = f_ω(N, M, kw)
+    m = f_m(N, M, kw)
+    l = f_l(N, M, d, kw)
+    u = f_u(N, M, kw)
 
-    ## Construct uniform and beta distribution to draw parameters from
-    dU = Uniform(0,1)
-    dB = Beta(4, 3)
-    dN = Normal(μ, 0.1*μ)
-    dΩ = Uniform(1, 10000)
+    λ = zeros(N, M)
 
-
-    ## Sample concentration parameters for each consumer from uniform distribution
-    if θ == nothing
-        θ = zeros(N, M)
-
-        for i in 1:N
-            θ[i, :] = rand(dU, M)
-        end
-    end
-
-    ## Sample specialisation parameter for each consumer
-    Ω = rand(dΩ, N)
-
-    ## Sample total uptake capacity per consumer
-    T = rand(dB, N)
-
-    ## Generate uptake matrix from a dirichlet distribution
-    for i in 1:N
-        dD = Dirichlet(Ω[i]*θ[i, :])
-        u[i, :] = rand(dD)*T[i]
-    end
-
-## Calculation of total row sum leakage from efficiencyt and dissipation
-# terms
-
+     ## Calculating total leakage of consumers per resource
     for i in 1:N
         for α in 1:M
-            L[i, α] = T[i]*(1-η[i, α]-d[i, α])
+            λ[i, α] = sum(l[i, α, :])
         end
     end
 
-
-
-
-    ## Generate leakage tensor from dirichlet distribution
-    for i in 1:N
-        dD = Dirichlet(Ω[i]*θ[i,:])
-        for α in 1:M
-            l[i, α, :] = rand(dD)*L[i, α]
-        end
+    if !haskey(kw, :d)
+        kw_nt = (; kwargs...)
+        p_nt = (N=N, M=M, u=u, m=m, l=l, ρ=ρ, ω=ω, λ=λ, d=d)
+    else
+        kw_nt = (; kwargs...)
+        p_nt = (N=N, M=M, u=u, m=m, l=l, ρ=ρ, ω=ω, λ=λ)
     end
 
-    ## Sample inflow parameter from uniform distribution
-    ρ = rand(M)*(1/M)
-    ω = rand(M)*(1/(2*M))
+    out = Base.merge(p_nt, kw_nt)
 
-    ## sample maitenance parameter from uniform distribution
-    m = [rand(dN)*T[i]^(2/3) for i in 1:N]
+    return out 
 
-    p = Dict(:l => l, :ρ => ρ, :ω => ω, :m => m, :M => M, :N => N, :u => u)
 end
